6afb0a22a4c60c9945f3fde0df519831208f1220
Yunika Bajracharya <076bct095.yunika@pcampus.edu.np>
2021-09-08 10:46:09 +0545

    Add js

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+// On clicking the button
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
